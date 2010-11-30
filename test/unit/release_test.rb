0bc8002331f2a7b9de0a69be89d4ad31d7041173
Dave Knapik <daveknapik@gmail.com>
2010-11-29 22:06:38 -0600

    Added test for release artist_name getter

--- test/unit/release_test.rb
+++ test/unit/release_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class ReleaseTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "get artist name" do
+    release = releases(:an_ark_for_the_listener)
+    assert_equal release.artist.name, release.artist_name
   end
 end
