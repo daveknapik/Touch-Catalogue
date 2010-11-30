4f05a5beb02e97a3c024c106b639d6f527a2c050
Dave Knapik <daveknapik@gmail.com>
2010-11-29 21:58:41 -0600

    Added simple artist-release relationship test

--- test/unit/artist_test.rb
+++ test/unit/artist_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class ArtistTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "Philip Jeck release count" do
+    philip_jeck = artists(:philip_jeck)
+    assert_equal 2, philip_jeck.releases.count
   end
 end
